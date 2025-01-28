cdfeb739d79f52b5f4978400dca9aea1dc9497d3
k-katayanagi <kkatayanagi.ka@gmail.com>
2025-01-28 17:08:24 +0900

    【ログインページ作成】
    login/page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,14 +2,23 @@
 'use client';
 
 import LoginButton from "@/components/buttons/LoginButton";
+import RegistrationButton from "@/components/buttons/RegistrationButton";
+import TopButton from "@/components/buttons/TopButton";
 
 const Login = () => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      ログインページです
-      <LoginButton/>
+    <div className="flex flex-col items-center justify-center w-full h-screen transform translate-y-[-60px]">
+      <div className="flex flex-col items-center justify-center w-full max-h-full overflow-y-auto sm:mt-0 md:mt-0">
+        <h1 className="mt-4 mb-4 font-bold">アカウントをお持ちの方はこちら</h1>
+        <LoginButton />
+        <div className="flex flex-col items-center justify-center mt-4">
+          <h1 className="mt-4 mb-4 font-bold">新規会員登録はこちら</h1>
+          <RegistrationButton />
+          <TopButton/>
+        </div>
+      </div>
     </div>
-  );
+    );
 }
 
 export default Login;
