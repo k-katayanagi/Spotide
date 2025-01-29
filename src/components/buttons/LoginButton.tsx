4e758c75e6f8511982ac04614eeb04aac46c05c3
k-katayanagi <kkatayanagi.ka@gmail.com>
2025-01-29 10:26:50 +0900

    【chakra-ui使用設定】
    使用ファイル
    providers.tsx
    LoginButton.tsx
    layout.tsx
    
    エラー内容
    プロパティ 'value' は型 '{ children: ReactNode; }' にありませんが、型 'ChakraProviderProps' では必須です。ts(2741)
    provider.d.ts(4, 5): 'value' はここで宣言されています。

--- src/components/buttons/LoginButton.tsx
+++ src/components/buttons/LoginButton.tsx
@@ -1,38 +1,50 @@
 'use client';
 
 import Button from '@components/Button';
-import { signIn } from 'next-auth/react';
+import { useToast } from '@chakra-ui/react';
+// import { signIn } from 'next-auth/react';
 import Image from 'next/image';
 
 type Props = {
-  imageWidth?: number;  // 画像の幅
-  imageHeight?: number; // 画像の高さ
+  imageWidth?: number;
+  imageHeight?: number;
 };
 
-const LoginButton = (props:Props) => {
-  const {imageWidth = 500,imageHeight= 500} = props
-  const handleLogin = () => {
-    console.log("Attempting Google SignIn");
-    signIn('google');  // Google認証を開始
-  }
+const LoginButton = (props: Props) => {
+  const { imageWidth = 500, imageHeight = 500 } = props;
+  const toast = useToast();
 
-  
+  const handleLogin = async () => {
+    console.log("Attempting Google SignIn");
+
+    // // Googleサインインを開始
+    // await signIn('google');
+    toast({
+      title: "ログインしました！",
+      description: "ログイン完了",
+      status: "success",
+      duration: 3000,
+      isClosable: true,
+      position: "top",
+    });
+  };
+
   return (
     <Button
-    onClick={handleLogin}
-    className="flex items-center justify-center w-full max-w-xs px-4 py-2 text-sm font-medium"
-    text={
-      <div className="flex items-center">
-        <Image
-          src="/images/web_light_rd_SI.svg"
-          alt="Googleでログイン"
-          width={imageWidth}
-          height={imageHeight}
-          className="mr-2 object-contain"
-        />
-      </div>
-    }
-  />
+      onClick={handleLogin}
+      className="flex items-center justify-center w-full max-w-xs px-4 py-2 text-sm font-medium"
+      text={
+        <div className="flex items-center">
+          <Image
+            src="/images/web_light_rd_SI.svg"
+            alt="Googleでログイン"
+            width={imageWidth}
+            height={imageHeight}
+            className="mr-2 object-contain"
+          />
+        </div>
+      }
+    />
   );
 };
 
