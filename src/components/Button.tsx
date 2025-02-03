2fc1ac1f7ee3d0838f914d84b2b885a1c9a5c918
k-katayanagi <kkatayanagi.ka@gmail.com>
2025-02-03 17:14:50 +0900

    【ログインボタン改修】
    LoginButton.tsx
    Button.tsx
    GoogleButton.css

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,34 +3,21 @@ import React from 'react';
 type ButtonProps = {
   text: React.ReactNode; // ボタン中身
   onClick: React.MouseEventHandler<HTMLButtonElement>; // onClick イベントの型
-  variant?: 'primary'
+
 } & React.ButtonHTMLAttributes<HTMLButtonElement>; // ボタン要素のその他の属性
 
-// variantが'primaryのCSSクラスを返す関数
-const getVariantClass = (variant: string) => {
-  switch (variant) {
-    case 'primary':
-      return 'btn-primary';
-    default:
-      return '';
-  }
-};
 
 const Button = (props: ButtonProps) => {
-  const { text, onClick, variant = '', className = '', ...restProps } = props;
+  const { text, onClick, className = '', ...restProps } = props;
 
-  // variantがある場合、事前に定義したクラスを取得
-  const variantClass = getVariantClass(variant);
- // variantクラスが指定されていればそれを優先し、なければclassNameを使う
-  const finalClassName = variantClass || className;
 
   return (
-    <button 
-      className={`btn ${finalClassName}`} // どちらかのクラスを適用
-      onClick={onClick} 
+    <button
+      className={`${className}`} 
+      onClick={onClick}
       {...restProps}
     >
-      {text}
+      {text} {/* ボタンの中身 */}
     </button>
   );
 };
