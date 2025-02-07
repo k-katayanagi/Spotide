362ef5d86c89d12550feadc6ee342c40fb42e9af
k-katayanagi <kkatayanagi.ka@gmail.com>
2025-02-07 11:56:01 +0900

    【修正】

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,13 +2,7 @@ import React from 'react';
 
 type ButtonProps = {
   text: React.ReactNode; // ボタン中身
-<<<<<<< HEAD
   onClick?: React.MouseEventHandler<HTMLButtonElement>; // onClick イベントの型
-  variant?: 'primary'
-=======
-  onClick: React.MouseEventHandler<HTMLButtonElement>; // onClick イベントの型
-
->>>>>>> 101fcf9bfafc362d7c7c421598ff51e4e64c5e60
 } & React.ButtonHTMLAttributes<HTMLButtonElement>; // ボタン要素のその他の属性
 
 
