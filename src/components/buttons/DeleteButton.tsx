23229e90326dbe3ec4bf45d0e3976155e7478fcb
k-katayanagi <kkatayanagi.ka@gmail.com>
2025-02-10 14:11:27 +0900

    【ボタン位置調整】

--- src/components/buttons/DeleteButton.tsx
+++ src/components/buttons/DeleteButton.tsx
@@ -11,13 +11,18 @@ const DeleteButton = ({ className }: Props) => {
   const { handleNavigateTo } = useNavigation();
   return (
     <Button
-      className={`group relative inline-flex items-center justify-center overflow-hidden rounded-full bg-gradient-to-r from-red-400 via-red-500 to-red-600 font-medium text-neutral-50 transition-all duration-300 hover:bg-gradient-to-r hover:from-pink-400 hover:via-pink-500 hover:to-pink-600 hover:shadow-2xl transform hover:scale-102 mt-5 ${className}`}
-      style={{ width: '200px', height: '50px' }}
+    className={`group relative inline-flex items-center justify-center overflow-hidden rounded-full 
+      bg-gradient-to-r from-red-400 via-red-500 to-red-600 
+      font-medium text-neutral-50 transition-all duration-300 
+      hover:bg-gradient-to-r hover:from-pink-400 hover:via-pink-500 hover:to-pink-600 
+      hover:shadow-2xl transform hover:scale-105 
+      mt-5 px-6 py-2 text-base md:text-lg lg:text-xl 
+      min-w-[30px] w-[100px] max-w-[100px] min-h-[10px] h-[50px]  max-h-[50px] whitespace-nowrap ${className}`}
       onClick={() => handleNavigateTo('/')}
       text={
         <>
           <span className="absolute h-0 w-0 rounded-full bg-pink-300 transition-all duration-300 group-hover:h-56 group-hover:w-32"></span>
-          <span className="relative flex items-center justify-center">削除</span> {/* 横並びに中央揃え */}
+          <span className="relative flex items-center justify-center">削除</span>
         </>
       }
     />
--- src/components/buttons/ViewingButton.tsx
+++ src/components/buttons/ViewingButton.tsx
@@ -11,13 +11,18 @@ const ViewingButton = ({ className }: Props) => {
   const { handleNavigateTo } = useNavigation();
   return (
     <Button
-      className={`group relative inline-flex items-center justify-center overflow-hidden rounded-full bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 font-medium text-neutral-50 transition-all duration-300 hover:bg-gradient-to-r hover:from-blue-400 hover:via-blue-500 hover:to-blue-600 hover:shadow-2xl transform hover:scale-102 mt-5 ${className}`}
-      style={{ width: '200px', height: '50px' }}
+      className={`group relative inline-flex items-center justify-center overflow-hidden rounded-full 
+        bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 
+        font-medium text-neutral-50 transition-all duration-300 
+        hover:bg-gradient-to-r hover:from-blue-400 hover:via-blue-500 
+        hover:to-blue-600 hover:shadow-2xl transform hover:scale-105
+        mt-5 px-6 py-2 text-base md:text-lg lg:text-xl
+        min-w-[30px] w-[100px] max-w-[100px] min-h-[10px] h-[50px]  max-h-[50px] whitespace-nowrap ${className}`}
       onClick={() => handleNavigateTo('/')}
       text={
         <>
           <span className="absolute h-0 w-0 rounded-full bg-blue-300 transition-all duration-300 group-hover:h-56 group-hover:w-32"></span>
-          <span className="relative flex items-center justify-center">閲覧</span> {/* 横並びに中央揃え */}
+          <span className="relative flex items-center justify-center">閲覧</span>
         </>
       }
     />
