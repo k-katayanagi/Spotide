3537795c40558d07c370a215f961bbe5e08205f2
k-katayanagi <kkatayanagi.ka@gmail.com>
2025-02-26 18:18:35 +0900

    【OKボタン変更】

--- src/components/buttons/OkButton.tsx
+++ src/components/buttons/OkButton.tsx
@@ -2,14 +2,16 @@
 
 import Button from '@components/Button';
 
-
 const OkButton = () => {
   return (
     <Button
-      className="group relative inline-flex h-16 items-center justify-center overflow-hidden rounded-full bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 px-9 font-medium text-neutral-50 transition-all duration-300 hover:bg-gradient-to-r hover:from-blue-400 hover:via-blue-500 hover:to-blue-600 hover:shadow-2xl transform hover:scale-102 mt-5"
+      className="group relative inline-flex h-16 items-center justify-center overflow-hidden rounded-full 
+                 bg-white border-4 border-[#FF5722] px-9 font-medium text-black transition-all duration-300 
+                 hover:bg-gradient-to-r hover:from-[#FF7043] hover:via-[#FF5722] hover:to-[#F4511E] 
+                 hover:shadow-2xl transform hover:scale-[1.02] mt-5"
       text={
         <>
-          <span className="absolute h-0 w-0 rounded-full bg-blue-300 transition-all duration-300 group-hover:h-56 group-hover:w-32"></span>
+          <span className="absolute h-0 w-0 rounded-full bg-[#FF8A65] transition-all duration-300 group-hover:h-56 group-hover:w-32"></span>
           <span>OK</span>
         </>
       }
