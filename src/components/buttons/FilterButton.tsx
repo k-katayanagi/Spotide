75e1e7e3bb1a273f080af325772a7226ad73e0f9
k-katayanagi <kkatayanagi.ka@gmail.com>
2025-03-11 00:31:52 +0900

    【update】フィルター/ソート画像配置

--- src/components/buttons/FilterButton.tsx
+++ src/components/buttons/FilterButton.tsx
@@ -1,22 +1,27 @@
 "use client";
 
-import Button from "@components/Button";
+import Button from "../Button";
+import Image from "next/image";
 
 interface Props {
   className?: string;
   disabled?: boolean;
-  onClick: () => void; // 親から渡されたクリック時のハンドラーを受け取る
+  onClick: () => void;
 }
 
 const FilterButton = ({ className, disabled, onClick }: Props) => {
   return (
     <Button
       className={`items-center justify-center text-black w-[25px] h-[25px] sm:w-[50px] sm:h-[50px] ${className}`}
-
       disabled={disabled}
-      onClick={onClick} // 親から渡されたクリックイベントを呼び出す
-      text={<>{"↓↑"}</>}
-    />
+      onClick={onClick}
+    >
+      <Image
+        src="/images/filter.svg"
+        alt="フィルターボタン"
+        className="w-[25px] h-[25px] sm:w-[50px] sm:h-[50px]"
+      />
+    </Button>
   );
 };
 
