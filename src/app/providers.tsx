7f173c33c1dcb7b10f0d223ac77581ccc5e2288c
shtk0llq <shtk0llq@gmail.com>
2025-03-22 07:59:13 +0900

    samplecode

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,20 +2,16 @@
 
 import { ChakraProvider } from "@chakra-ui/react";
 import { BottomNavProvider } from "@/contexts/BottomNavContext";
-import { NextAuthProvider } from "@/lib/NextAuthProvider";
-import { Session } from "next-auth";
 
 export function Providers({
   children,
-  session,
 }: {
   children: React.ReactNode;
-  session: Session | null; // sessionを受け取る
 }) {
   return (
     <ChakraProvider>
       <BottomNavProvider>
-        <NextAuthProvider session={session}>{children}</NextAuthProvider> {/* sessionを渡す */}
+        {children}
       </BottomNavProvider>
     </ChakraProvider>
   );
---
+++ src/auth.ts
@@ -0,0 +1,62 @@
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import { supabase } from "./lib/supabase";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [Google],
+  session: { strategy: "jwt" },
+  callbacks: {
+    async signIn({ user, account }) {
+      try {
+        const { data: existingUser } = await supabase
+          .from("users")
+          .select()
+          .eq("google_id", account?.providerAccountId)
+          .single();
+
+        if (!existingUser) {
+          await supabase
+            .from("users")
+            .insert({
+              google_id: account?.providerAccountId,
+              user_name: user.name,
+            })
+            .single();
+        }
+
+        return true;
+      } catch (error) {
+        console.error(error);
+        return false;
+      }
+    },
+
+    async session({ session, token }) {
+      if (token?.googleId) {
+        try {
+          const { data: user } = await supabase
+            .from("users")
+            .select("user_id")
+            .eq("google_id", token.googleId)
+            .single();
+
+          if (user) {
+            session.user.id = user.user_id;
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      }
+
+      return session;
+    },
+
+    async jwt({ token, user, account }) {
+      if (user && account) {
+        token.googleId = account.providerAccountId;
+      }
+
+      return token;
+    },
+  },
+});
