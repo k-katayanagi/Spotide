ce39cc27c88d8c48f81093b9054f0e794dee6d4b
[k-katayanagi] <[kkatayanagi.ka@gmail.com]>
2025-03-30 11:27:45 +0900

    【update】registryをloginに集約

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,22 +1,37 @@
-'use client';
+"use client";
 
+import { signIn } from "next-auth/react";
+import { useState } from "react";
 import LoginButton from "@/components/buttons/LoginButton";
-import RegistrationButton from "@/components/buttons/RegistrationButton";
 import TopButton from "@/components/buttons/TopButton";
 
-const Login = () => {
+const  Login = () => {
+  const [errorMessage, setErrorMessage] = useState("");
+  
+  // ログイン処理
+  const handleLogin = async () => {
+    console.log("Googleアカウントでログイン開始");
+
+    const result = await signIn("google", {
+      callbackUrl: "/user/mypage",
+    });
+
+    if (result?.error) {
+      setErrorMessage(`ログインに失敗しました: ${result.error}`);
+      return;
+    }
+
+  };
+
   return (
     <div className="flex flex-col items-center justify-center w-full h-screen transform translate-y-[-60px] space-y-12 sm:space-y-16 md:space-y-20 lg:space-y-24">
       <div className="flex flex-col items-center justify-center w-full max-h-full overflow-y-auto sm:mt-0 md:mt-0 space-y-1">
-        <h1 className="mt-4 mb-3 font-bold text-lg sm:text-xl md:text-2xl">アカウントをお持ちの方はこちら</h1>
-        <LoginButton />
+        <h1 className="mt-4 mb-3 font-bold text-lg sm:text-xl md:text-2xl">
+          ログインはこちら
+        </h1>
+        <LoginButton onClick={handleLogin} />
+        {errorMessage && <div className="text-red-500">{errorMessage}</div>}
       </div>
-
-      <div className="flex flex-col items-center justify-center mt-4 space-y-1">
-        <h1 className="mt-4 mb-3 font-bold text-lg sm:text-xl md:text-2xl">新規会員登録はこちら</h1>
-        <RegistrationButton />
-      </div>
-
       <div className="flex flex-col items-center justify-center mt-4 space-y-1">
         <TopButton />
       </div>
